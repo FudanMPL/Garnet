666eb950dcb8edbb329cc7ed0b97324bfca01874
XinLin-cs <1004150336@qq.com>
2023-07-12 10:58:35 +0800

    tensor sum

--- Compiler/tensor.py
+++ Compiler/tensor.py
@@ -458,8 +458,42 @@ class Tensor():
         return self
 
     def mean(self):
-        #todo
-        return self
+        # backward
+        @buildingblock(get_program().globalbuildingblock)
+        def propagate(dl_doutputs, operation):
+            dl_dx, = dl_doutputs
+            inputs = operation.inputs
+            dl_dself = dl_d[inputs[0]]
+            for si in self.value.sizes:
+                dl_dself[:] += dl_dx[0] / si
+            dl_dinputs = [dl_dself]
+            return dl_dinputs
+        # forward
+        global op_id
+        if prepare:    
+            new_value = Array(1, self.value.value_type)
+            output = Tensor(new_value)
+            
+            operation = Operation(inputs=[self.name], outputs=[output.name], propagate=propagate)
+            gradient_operation.append(operation)
+            operation_id = len(gradient_operation) - 1
+            
+            op_id_store[op_id] = operation_id
+            op_id += 1
+        else:
+            operation = gradient_operation[op_id_store[op_id]]
+            inputs = operation.inputs
+            outputs = operation.outputs
+            input = tensors[inputs[0]]
+            output = tensors[outputs[0]]
+
+            output.value[:] = sum(input.value[:])
+            for si in self.value.sizes:
+                output.value[:] =  output.value[:] / si
+            
+            op_id += 1
+        # record the input and output of the op
+        return output
 
     def sum(self):
         # backward
