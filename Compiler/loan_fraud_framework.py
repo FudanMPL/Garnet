d2cfbd86685e20ce055989010a558c04b96710ee
林国鹏 <17302010022@fudan.edu.cn>
2023-07-14 21:02:54 +0800

    完成贷款诈骗类案监督代码

--- Compiler/loan_fraud_framework.py
+++ Compiler/loan_fraud_framework.py
@@ -3,11 +3,11 @@ from Compiler.sorting import *
 from Compiler.group_ops import *
 
 
-class LoanFraudAnalysis:
-    def __init__(self, party_number, partys_sample_number, feature_number, model):
+class LoanFraudFramework:
+    def __init__(self, party_number, sample_number_of_parties, feature_number, model):
         self.party_number = party_number
-        self.partys_sample_number = partys_sample_number
-        self.sample_number = sum(partys_sample_number)
+        self.sample_number_of_parties = sample_number_of_parties
+        self.sample_number = sum(sample_number_of_parties)
         self.feature_number = feature_number
         self.data_before_merge = sint.Matrix(self.sample_number, self.feature_number)
         self.model = model
@@ -16,11 +16,11 @@ class LoanFraudAnalysis:
         self.model = model
 
 
-    def share_data(self):
+    def input_data_from_parties(self):
         count = 0
         party_id = 0
         for x in self.data_before_merge:
-            if count == self.sample_number[self.partys_sample_number[party_id] * self.sample_number]:
+            if count == self.sample_number_of_parties[party_id]:
                 count = 0
                 party_id = party_id + 1
             x.input_from(party_id)
@@ -31,21 +31,32 @@ class LoanFraudAnalysis:
         perm = gen_perm_by_radix_sort(key)
         self.data = sint.Matrix(self.sample_number, self.feature_number)
         for i in range(self.feature_number):
-            self.data.set_column(i, perm.apply(self.data_before_merge.get_column(i)))
-        flag = sint(0, self.sample_number)
+            col = perm.apply(self.data_before_merge.get_column(i))
+            self.data.set_column(i, col.get_vector())
+        flag = sint.Array(size=self.sample_number)
         flag[0] = 1
         key = self.data.get_column(0)
         for i in range(1, self.sample_number):
             flag[i] = key[i] != key[i-1]
-        for i in range(self.feature_number):
+        for i in range(1, self.feature_number):
             self.data.set_column(i, GroupSum(flag, self.data.get_column(i)))
         for i in range(self.sample_number):
             self.data[i] = self.data[i].get_vector() * flag[i]
-        perm = SortPerm(1 - flag)
+        perm = SortPerm(flag.get_vector().bit_not())
         for i in range(self.feature_number):
-            self.data.set_column(i, perm.apply(self.data.get_column(i)))
+            self.data.set_column(i, perm.apply(self.data.get_column(i)).get_vector())
 
     def predict_and_reveal(self):
-        predict = self.model(self.data)
-        res = predict * self.data.get_column(0)
-        print(res.reveal())
+        predict = sint.Array(self.sample_number)
+        for i in range(self.sample_number):
+            predict[i] = self.model(self.data[i])
+        pred_res = predict * self.data.get_column(0)
+        perm = SortPerm(predict.get_vector().bit_not())
+        pred_res = perm.apply(pred_res)
+        res = pred_res.reveal()
+        print_str("fraud company id: ")
+        for id in res:
+            @if_(id != 0)
+            def _():
+                print_str("%s ", id)
+        print_ln("")
