f9e03e724d40d51fb7efe712ce4318ca97479ce0
林国鹏 <17302010022@fudan.edu.cn>
2024-02-21 17:05:38 +0800

    add debug breakpoint function for mpc

--- Compiler/library.py
+++ Compiler/library.py
@@ -2093,4 +2093,9 @@ def ss_psi_merge(*tables):
         final_table.set_column(i, perm.apply(final_table.get_column(i)).get_vector())
     return final_table, sum(in_intersection)
 
+def debug_point():
+    """
+    Insert debug point for debug with ide
+    """
+    return
 
