d4c711ec9b7da57adc5594d0bd30153086137a15
Crypto-zhou <201800122031@mail.sdu.edu.cn>
2025-03-17 22:31:01 +0800

    ann

---
+++ LAN.sh
@@ -0,0 +1,15 @@
+sudo tc qdisc del dev lo root
+sudo tc qdisc add root dev lo handle 1: htb default 7
+sudo tc class add dev lo parent 1:0 classid 1:1 htb rate 128Mbps
+sudo tc class add dev lo parent 1:0 classid 1:2 htb rate 128Mbps
+sudo tc class add dev lo parent 1:0 classid 1:3 htb rate 128Mbps
+sudo tc class add dev lo parent 1:0 classid 1:4 htb rate 128Mbps
+sudo tc class add dev lo parent 1:0 classid 1:5 htb rate 128Mbps
+sudo tc class add dev lo parent 1:0 classid 1:6 htb rate 128Mbps
+sudo tc class add dev lo parent 1:0 classid 1:7 htb rate 128Mbps
+sudo tc filter add dev lo protocol ip parent 1:0 prio 1 u32 match ip sport 10000 0xffff flowid 1:1
+sudo tc filter add dev lo protocol ip parent 1:0 prio 1 u32 match ip dport 10000 0xffff flowid 1:2
+sudo tc filter add dev lo protocol ip parent 1:0 prio 1 u32 match ip sport 10001 0xffff flowid 1:3
+sudo tc filter add dev lo protocol ip parent 1:0 prio 1 u32 match ip dport 10001 0xffff flowid 1:4
+sudo tc filter add dev lo protocol ip parent 1:0 prio 1 u32 match ip sport 10002 0xffff flowid 1:5
+sudo tc filter add dev lo protocol ip parent 1:0 prio 1 u32 match ip dport 10002 0xffff flowid 1:6
